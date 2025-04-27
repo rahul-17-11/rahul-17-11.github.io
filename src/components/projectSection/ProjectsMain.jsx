ea243688ddb165168a6d86e0d577ae8f1a804623
Rahul Waghmare <rahulwaghmare1711@gmail.com>
2025-04-27 22:53:37 +0530

    project added

--- src/components/projectSection/ProjectsMain.jsx
+++ src/components/projectSection/ProjectsMain.jsx
@@ -1,71 +1,87 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
-import ProjectsText from './ProjectsText'
-import SingleProject from './SingleProject'
-import { motion } from 'framer-motion'
-import { fadeIn } from '../../framerMotion/variants'
+import React from "react";
+import ProjectsText from "./ProjectsText";
+import SingleProject from "./SingleProject";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../framerMotion/variants";
 
 const projects = [
-    {
-      name:'EduQuizz',
-      year:'Feb2025',
-      align:'right',
-      image:'/images/eduquizz.png',
-      link:'https://educationquizzapp.netlify.app/',
-      gitLink:"https://github.com/Aamir-Hamza/B42_WEB_040_Responsive-Riders"
-    },
-    {
-      name:'ShaktiMudra',
-      year:'Mar2025',
-      align:'left',
-      image:'/images/shaktimudra.png',
-      link:'https://shaktimudra.netlify.app/',
-      gitLink:"https://github.com/rahul-17-11/shaktimudra"
-    },
-    {
-      name:'WeatherApp',
-      year:'Jan2025',
-      align:'right',
-      image:'/images/weatherapp.png',
-      link:'https://rahul-my-weather-app.netlify.app/',
-      gitLink:"https://github.com/rahul-17-11/my-weatherapp"
-    },
-    {
-      name:'PasswordGenerator',
-      year:'April2024',
-      align:'left',
-      image:'/images/password.png',
-      link:'https://rahul17-password-generator.netlify.app/',
-      gitLink:"https://github.com/rahul-17-11/passGenrator"
-    }
-]
+  {
+    name: "Interview AI",
+    year: "Feb2025",
+    align: "right",
+    image: "/images/interview-ai.png",
+    link: "https://interview-ai-psi-lac.vercel.app/sign-in",
+    gitLink: "https://github.com/rahul-17-11/interview-ai",
+  },
+  {
+    name: "EduQuizz",
+    year: "Feb2025",
+    align: "left",
+    image: "/images/eduquizz.png",
+    link: "https://educationquizzapp.netlify.app/",
+    gitLink: "https://github.com/Aamir-Hamza/B42_WEB_040_Responsive-Riders",
+  },
+  {
+    name: "ShaktiMudra",
+    year: "Mar2025",
+    align: "right",
+    image: "/images/shaktimudra.png",
+    link: "https://shaktimudra.netlify.app/",
+    gitLink: "https://github.com/rahul-17-11/shaktimudra",
+  },
+  {
+    name: "WeatherApp",
+    year: "Jan2025",
+    align: "left",
+    image: "/images/weatherapp.png",
+    link: "https://rahul-my-weather-app.netlify.app/",
+    gitLink: "https://github.com/rahul-17-11/my-weatherapp",
+  },
+  {
+    name: "PasswordGenerator",
+    year: "April2024",
+    align: "right",
+    image: "/images/password.png",
+    link: "https://rahul17-password-generator.netlify.app/",
+    gitLink: "https://github.com/rahul-17-11/passGenrator",
+  },
+];
 
 const ProjectsMain = () => {
   return (
-    <motion.div 
-    variants={fadeIn('down',0.2)}
-                initial='hidden'
-                whileInView='show'
-                viewport={{once:false, amount:0}}
-    id='projects' className='max-w-[1200px] mx-auto px-4'>
+    <motion.div
+      variants={fadeIn("down", 0.2)}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0 }}
+      id="projects"
+      className="max-w-[1200px] mx-auto px-4"
+    >
       <ProjectsText />
-      <div className='flex flex-col gap-20 max-w-[900px] mx-auto mt-12'>
-        {
-          projects.map((ele,i)=>(
-              <motion.div 
-                variants={fadeIn(`${ele.align=='left'?'right':'left'}`,0.2)}
-                initial='hidden'
-                whileInView='show'
-                viewport={{once:false, amount:0}}
-                key={i}             
-                >
-            <SingleProject key={i} name={ele.name} year={ele.year} align={ele.align} image={ele.image} link={ele.link} gitLink={ele.gitLink} />
-              </motion.div>
-          ))
-        }
+      <div className="flex flex-col gap-20 max-w-[900px] mx-auto mt-12">
+        {projects.map((ele, i) => (
+          <motion.div
+            variants={fadeIn(`${ele.align == "left" ? "right" : "left"}`, 0.2)}
+            initial="hidden"
+            whileInView="show"
+            viewport={{ once: false, amount: 0 }}
+            key={i}
+          >
+            <SingleProject
+              key={i}
+              name={ele.name}
+              year={ele.year}
+              align={ele.align}
+              image={ele.image}
+              link={ele.link}
+              gitLink={ele.gitLink}
+            />
+          </motion.div>
+        ))}
       </div>
     </motion.div>
-  )
-}
+  );
+};
 
-export default ProjectsMain
+export default ProjectsMain;
